2b4cc2f644c4cb285b23c468f71fba9147ab2492
H.Vasev <hristovasev999@gmail.com>
2024-07-26 09:56:09 +0100

    Add project files.

---
+++ gym-Api/Controllers/TrainingController.cs
@@ -0,0 +1,33 @@
+﻿using gym_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace gym_Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class TrainingController : Controller
+    {
+        [HttpGet("training")]
+        public IActionResult GetVideos()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","videos");
+            var videosFiles = Directory.GetFiles(filePath, "*.mp4");
+
+            var videos = new List<VideoFile>();
+
+            foreach (var video in videosFiles)
+            {
+                var fileName = Path.GetFileName(video);
+                var videoUrl = $"https://localhost:7010/videos/{fileName}";
+                videos.Add(new VideoFile()
+                {
+                    FileName = fileName,
+                    Url = videoUrl
+                });
+            }
+
+            return Ok(videos);
+        }
+    }
+}
---
+++ gym-Api/Models/VideoFile.cs
@@ -0,0 +1,9 @@
+﻿namespace gym_Api.Models
+{
+    public class VideoFile
+    {
+        public string FileName { get; set; }
+
+        public string Url { get; set; }
+    }
+}
